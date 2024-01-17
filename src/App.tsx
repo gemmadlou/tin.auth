a9f23451bb53a1884742303ada41775ea0aca9c5
Gemma Black <gblackuk@googlemail.com>
2024-01-17 08:51:55 +0000

    Add Clerkjs sign in button

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { SignedOut, SignedIn } from "@clerk/clerk-react";
+import { SignedOut, SignedIn, SignInButton } from "@clerk/clerk-react";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,8 @@ function App() {
   return (
     <>
       <SignedOut>
-        Must sign in
+        Must sign in.
+        <SignInButton />
       </SignedOut>
       <SignedIn>
         <div>
